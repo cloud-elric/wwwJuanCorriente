1783d5ac959ec7cc4e98e85685ffe86a15f61783
damsSanch <damian2gom@gmail.com>
2016-10-24 15:55:28 -0500

    Ver capítulo - imagenes(small-large) centradas

--- views/site/verCapitulo.php
+++ views/site/verCapitulo.php
@@ -48,11 +48,24 @@ use yii\helpers\Url;
 			<p>
 				Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag typewriter banh mi, squid keffiyeh High Life Brooklyn twee craft beer tousled chillwave. PBR&B selfies chillwave, bespoke tote bag blog post-ironic. Single-origin coffee put a bird on it craft beer YOLO, Portland hella deep v Schlitz. Tumblr Bushwick post-ironic Thundercats. Vinyl 90's keytar, literally cardigan Williamsburg YOLO squid pickled Etsy salvia lo-fi locavore. Meh leggings retro narwhal Neutra.
 			</p>
-			<img class="home-article-imagen" src="<?=Url::base()?>/webAssets/images/portada.jpg" alt="Article">
+			<div class="ver-capitulo-post-imagen">
+				<img src="<?=Url::base()?>/webAssets/images/portada.jpg" alt="Article">
+			</div>
 			<p>
 				Curabitur lobortis id lorem id bibendum. Ut id consectetur magna. Quisque volutpat augue enim, pulvinar lobortis nibh lacinia at. Vestibulum nec erat ut mi sollicitudin porttitor id sit amet risus. Nam tempus vel odio vitae aliquam. In imperdiet eros id lacus vestibulum vestibulum. Suspendisse fermentum sem sagittis ante venenatis egestas quis vel justo. Maecenas semper suscipit nunc, sed aliquam sapien convallis eu. Nulla ut turpis in diam dapibus consequat.
 			</p>
-			<img class="home-article-imagen" src="<?=Url::base()?>/webAssets/images/portada.jpg" alt="Article">
+			<div class="ver-capitulo-post-imagen">
+				<img src="<?=Url::base()?>/webAssets/images/2gomLogo.png" alt="Article">
+			</div>
+			<p>
+				It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends; fast to Queequeg's broad canvas belt, and fast to my narrow leather one. So that for better or for worse, we two, for the time, were wedded; and should poor Queequeg sink to rise no more, then both usage and honour demanded, that instead of cutting the cord, it should drag me down in his wake. So, then, an elongate.
+			</p>
+			<p>
+				Banjo tote bag bicycle rights, High Life sartorial cray craft beer whatever street art fap. Hashtag typewriter banh mi, squid keffiyeh High Life Brooklyn twee craft beer tousled chillwave. PBR&B selfies chillwave, bespoke tote bag blog post-ironic. Single-origin coffee put a bird on it craft beer YOLO, Portland hella deep v Schlitz. Tumblr Bushwick post-ironic Thundercats. Vinyl 90's keytar, literally cardigan Williamsburg YOLO squid pickled Etsy salvia lo-fi locavore. Meh leggings retro narwhal Neutra.
+			</p>
+			<div class="ver-capitulo-post-imagen">
+				<img src="<?=Url::base()?>/webAssets/images/monkey.png" alt="Article">
+			</div>
 			<p>
 				It was a humorously perilous business for both of us. For, before we proceed further, it must be said that the monkey-rope was fast at both ends; fast to Queequeg's broad canvas belt, and fast to my narrow leather one. So that for better or for worse, we two, for the time, were wedded; and should poor Queequeg sink to rise no more, then both usage and honour demanded, that instead of cutting the cord, it should drag me down in his wake. So, then, an elongate.
 			</p>
