4e30c7aff89bdeee9f12b951c711a19376bf9b00
cloud-elric <humberto@2gom.com.mx>
2016-10-25 11:40:02 -0500

    Acomodando los elementos

--- views/site/verCapitulo.php
+++ views/site/verCapitulo.php
@@ -1,27 +1,41 @@
 <?php
 
 /* @var $this yii\web\View */
-
 $this->title = 'Ver Capítulo';
 use yii\helpers\Url;
+use app\models\EntElementos;
+use app\models\ConstantesWeb;
 
-$this->registerJsFile ( '@web/js/admin.js', [
-		'depends' => [
-				\app\assets\AppAsset::className ()
-		]
+$this->registerJsFile ( '@web/js/admin.js', [ 
+		'depends' => [ 
+				\app\assets\AppAsset::className () 
+		] 
 ] );
 
+$header = EntElementos::find ()->where ( [ 
+		'id_capitulo' => $capitulo->id_capitulo,
+		'b_header' => 1,
+		'id_tipo_elemento' => ConstantesWeb::TIPO_ELEMENTO_HEADER 
+] )->one ();
+$editable = '';
+$classEditable = '';
+$isAdmin = true;
+if ($isAdmin) {
+	$editable = " contentEditable='true' data-new='noNuevo'  data-progress='noProceso' ";
+	$classEditable = ' js-elemento-editable';
+}
 ?>
-<input type="hidden" data-token="<?=$capitulo->txt_token?>" id="js-capitulo" />
+<input type="hidden" data-token="<?=$capitulo->txt_token?>"
+	id="js-capitulo" />
 <!-- .ver-capitulo -->
 <div class="ver-capitulo" id="specialstuff">
 
 	<!-- .ver-capitulo-header -->
-	<div class="ver-capitulo-header">
+	<div class="ver-capitulo-header" style="background-image: url(<?=Url::base().(empty($header)?'/webAssets/images/portada.jpg':$header->txt_valor)?>)">
 
 		<h1>Historias de México</h1>
 
-		<h2>El Milagro</h2>
+		<h2><?=$capitulo->txt_nombre?></h2>
 
 		<!-- Leer de día o noche -->
 		<i class="ion ion-ios-book ver-capitulo-leer"></i>
@@ -46,13 +60,14 @@ $this->registerJsFile ( '@web/js/admin.js', [
 
 		<!-- .ver-capitulo-post -->
 		<div class="ver-capitulo-post">
-		<?php 
-		foreach($elementos as $elemento){
-			if(!$elemento->b_header){
-			?>
-		<div class='js-elemento-editable' contentEditable='true' data-new='noNuevo' data-token="<?=$elemento->txt_token?>" data-progress='noProceso'><?=$elemento->txt_valor?></div>
+		<?php
+		foreach ( $elementos as $elemento ) {
+			if (! $elemento->b_header) {
+				?>
+		<div class='<?=$classEditable?>' <?=$editable?>
+				data-token="<?=$elemento->txt_token?>"><?=$elemento->txt_valor?></div>
 		
-		<?php 
+		<?php
 			}
 		}
 		?>
@@ -69,20 +84,24 @@ $this->registerJsFile ( '@web/js/admin.js', [
 </div>
 <!-- end - .ver-capitulo -->
 
-<!-- <input type="button" value="Go Fullscreen" id="fsbutton" /> -->
+<?php
+if ($isAdmin) {
+	?>
+<!-- .modal-admin-form-controlers -->
+<div class="modal-admin-form-controlers">
 
-		<!-- .modal-admin-form-controlers -->
-		<div class="modal-admin-form-controlers">
+	<button
+		class="btn modal-admin-form-controlers-btn-circle modal-admin-form-controlers-btn-circle-texto js-texto">
+		<i class="ion ion-document-text"></i>
+	</button>
 
-			<button 
-				class="btn modal-admin-form-controlers-btn-circle modal-admin-form-controlers-btn-circle-texto js-texto">
-				<i class="ion ion-document-text"></i>
-			</button>
+	<button
+		class="btn modal-admin-form-controlers-btn-circle modal-admin-form-controlers-btn-circle-imagen">
+		<i class="ion ion-image"></i>
+	</button>
 
-			<button 
-				class="btn modal-admin-form-controlers-btn-circle modal-admin-form-controlers-btn-circle-imagen">
-				<i class="ion ion-image"></i>
-			</button>
-
-		</div>
-		<!-- end - .modal-admin-form-controlers -->
+</div>
+<!-- end - .modal-admin-form-controlers -->
+<?php
+}
+?>		
