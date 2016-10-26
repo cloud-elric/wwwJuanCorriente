090b0c0d36e5591af3cb227fcb4fd960ec7496ec
cloud-elric <humberto@2gom.com.mx>
2016-10-26 13:36:07 -0500

    Revert "Listado - file (30%)"
    This reverts commit 01e26c5aab3a968dfb9b26494cf96a8881395765.

--- views/site/verCapitulo.php
+++ views/site/verCapitulo.php
@@ -108,8 +108,7 @@ if ($isAdmin) {
 			
 				<input type="file" class="modal-admin-form-imagen" onchange="uploadImage($(this),this)"> 
 				 -->
-			
-			<!-- .ver-capitulo-post-image -->
+
 			<div class="ver-capitulo-post-image ver-capitulo-post-hover-close">
 				<div class="ver-capitulo-post-image-item js-container-image">
 					<!-- Input -->
@@ -127,9 +126,7 @@ if ($isAdmin) {
 					<span class="ver-capitulo-post-hover-close-btn"><i class="ion ion-close-round"></i></span>
 				</div>
 			</div>
-			<!-- end .ver-capitulo-post-image -->
-			
-			<!-- .ver-capitulo-post-image -->
+
 			<div class="ver-capitulo-post-image ver-capitulo-post-hover-close">
 				<div class="ver-capitulo-post-image-item js-container-image ver-capitulo-post-image-item-file">
 					<input type="file" id="file" class="inputfile modal-admin-form-imagen" onchange="uploadImage($(this),this)">
@@ -138,7 +135,6 @@ if ($isAdmin) {
 					<span class="ver-capitulo-post-hover-close-btn"><i class="ion ion-close-round"></i></span>
 				</div>
 			</div>
-			<!-- end - .ver-capitulo-post-image -->
 
 		</div>
 		<!-- end - .ver-capitulo-post -->
